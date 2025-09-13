17f5cca1c93e736f0908e68fb3937989ee7d3ac8
camper123-noob <arnav2005k@gmail.com>
2025-09-13 06:00:51 -0600

    Heavily Brought down time

--- Backend/README.md
+++ Backend/README.md
@@ -1,2 +1,4 @@
 Run main.py to get both the transcript and the scenes
 In order to try a different song run youtube.py with the correct link and delete the old m3p file
+Completed in 25 seconds
+Runs to local host Port:5000
---
+++ Backend/ytapi.py
@@ -0,0 +1,54 @@
+import requests
+import os
+import subprocess
+import sys
+def getmp3(url):
+    subprocess.run([sys.executable, "-m", "yt_dlp","-x","--audio-format", "mp3","--no-playlist","-o","--cookies-from-browser" r"%(title)s.%(ext)s",url])
+
+def getYTqueries(searchQuery:str)->list[dict[str:str]]:
+    """Allows you to run youtube search queries will
+    return the author title and url
+    """
+    API_KEY = os.getenv("GEMINI_API_KEY") # replace with your key
+    url = "https://www.googleapis.com/youtube/v3/search"
+    params = {
+        "part": "snippet",
+        "q": searchQuery,
+        "type": "video",
+        "maxResults": 5,
+        "key": API_KEY
+    }
+
+    resp = requests.get(url, params=params)
+    lst=[]
+    data = resp.json()
+    for item in data.get("items", []):
+        video_id = item["id"]["videoId"]
+        title = item["snippet"]["title"]
+        author = item["snippet"]["channelTitle"]
+        lst.append({"Title":title,"Author":author,"url":f"https://www.youtube.com/watch?v={video_id}"})
+    return lst
+def getYTurl(searchQuery):
+    """Allows you to run youtube search queries will
+    return the author title and url
+    """
+    API_KEY = os.getenv("GEMINI_API_KEY") # replace with your key
+    url = "https://www.googleapis.com/youtube/v3/search"
+    params = {
+        "part": "snippet",
+        "q": searchQuery,
+        "type": "video",
+        "maxResults": 1,
+        "key": API_KEY
+    }
+
+    resp = requests.get(url, params=params)
+    lst=[]
+    data = resp.json()
+    for item in data.get("items", []):
+        video_id = item["id"]["videoId"]
+        return f"https://www.youtube.com/watch?v={video_id}"
+
+if __name__=="__main__":
+    x=getYTurl("Play Lose You-Drake")
+    print(x)
